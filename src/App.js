2bcbdd7420a367178d6724482c438e1ba9040ecd
Kausalya.K <85003124+kausalya17897@users.noreply.github.com>
2022-03-21 01:31:31 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@
 import './App.css';
 import Formfull from './Component/Formfull';
 import Table from './Component/Table';
-import {Switch,Route} from 'react-router-dom'
+import {Switch,Route,Link} from 'react-router-dom'
 import {Editform} from './Component/Editform';
+import Welcome from './Welcome'
 
 function App() {
   return (
     <div className="App">
+      <Link to="/" className='head'>Home</Link>
+      <Link to="/employee/" className='head'>Registrationform</Link>
       <Switch>
-        <Route exact path="/"></Route>
+        <Route exact path="/"><Welcome/></Route>
         <Route  exact path="/employee/edit:id"><Editform/>fgdh</Route>
         <Route exact path="/employee/:id"></Route>
         <Route exact path="/employee"> <Formfull/>
