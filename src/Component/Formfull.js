5f267c615bc60ddc911bdb9edbce7fa5b2eb03d5
Kausalya.K <85003124+kausalya17897@users.noreply.github.com>
2022-03-24 14:44:08 +0530

    final update

--- src/Component/Formfull.js
+++ src/Component/Formfull.js
@@ -1,166 +1,184 @@
-import React from 'react';
-import './Form.css';
-import { useState } from 'react';
-import {  Row, Col,Form,Container,Card } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Select,MenuItem ,Button,} from '@mui/material';
-import { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import Table from './Table';
+import React from "react";
+import "./Form.css";
+import { useState } from "react";
+import { Row, Col, Form, Container, Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Select, MenuItem, Button } from "@mui/material";
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import Table from "./Table";
 
 export default function Formfull() {
-  const [countrycode, setCountrycode] = useState('');
-  const [jobtype, setJobtype] = useState('');
-  const [location, setLocation] =useState('');
- const [fullname,setFullname]=useState('');
- const [dob,setDob]=useState('');
- const [img,setImg]=useState("");
- const [email,setEmail]=useState("")
- const [mobile,setMobile]=useState("");
- const history=useHistory();
+  const [countrycode, setCountrycode] = useState("");
+  const [jobtype, setJobtype] = useState("");
+  const [location, setLocation] = useState("");
+  const [fullname, setFullname] = useState("");
+  const [dob, setDob] = useState("");
+  const [img, setImg] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
+  const history = useHistory();
 
- const addemployee=()=>{
-   console.log("adding...",fullname,email,mobile,dob,location,jobtype,img)
-   const newEmployee={
-    fullname,email,mobile,dob,location,jobtype,img
-   }
-   //fetch(`https://62368e85163bf7c4746552f6.mockapi.io/employee`,{
-     fetch(`https://paripornaform.herokuapp.com/employee`,{
-     method:"POST",
-     body:JSON.stringify(newEmployee),
-     headers:{
-       "content-Type":"application/json",
-     },
-   }).then(()=>history.push("/employee"))
-  }
+  const addemployee = () => {
+    console.log(
+      "adding...",
+      fullname,
+      email,
+      mobile,
+      dob,
+      location,
+      jobtype,
+      img
+    );
+    const newEmployee = {
+      fullname,
+      email,
+      mobile,
+      dob,
+      location,
+      jobtype,
+      img,
+    };
+    //fetch(`https://62368e85163bf7c4746552f6.mockapi.io/employee`,{
+    fetch(`https://paripornaform.herokuapp.com/employee`, {
+      method: "POST",
+      body: JSON.stringify(newEmployee),
+      headers: {
+        "content-Type": "application/json",
+      },
+    }).then(() => history.push("/employee"));
+  };
   useEffect(() => {
-    addemployee() 
-  }, [])
- /*const handleChange=(a)=>{
+    addemployee();
+  }, []);
+  /*const handleChange=(a)=>{
    if(a.target.files.length!==0){
      setImg(URL.createObjectURL(a.target.files[0]))
     }
   }*/
 
   return (
-    <div className='Form'>
-    
-    <Form>
-    <fieldset className='fieldset'>
-      <legend className="legend">Registration</legend>
-      <Container>
-  <Row>
-    <Col xl={6} className="fullnamecontainer">
-      <html for="fullname la">Fullname</html>
-      <input className='fullname' 
-      type="text"
-      value={fullname}
-      onChange={(a)=>setFullname(a.target.value)}/>
-    
-    </Col>
-    <Col xl={6} className="img">
-      <html for="profilepic la">Profile Pic</html>
-      <Card className='imagebox'>
-        
-        <img  className="image"src={img} 
-        alt="profilepic"/>
-        
-      </Card>
-      <input type="file"
-      accept='image/*'
-      value={img}
-      onChange={(a)=>setImg(a.target.value)}
-      />
-    </Col>
-  </Row>
-  <Row>
-    <Col xl={6} className="mobilecontainer input">
-      <html for="mobile la">Mobile</html>
-      <Select
-          labelId="demo-simple-select-required-html"
-          id="demo-simple-select-required"
-          value={countrycode}
-          html="Age *"
-          onChange={(event)=>setCountrycode(event.target.value)}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={+91}>+91</MenuItem>
-          <MenuItem value={+47}>+47</MenuItem>
-          <MenuItem value={+385}>+385</MenuItem>
-        </Select>
-      <input className='Mobile' type="text"
-      value={mobile}
-      onChange={(a)=>setMobile(a.target.value)}/>
-    
-    </Col>
-    <Col xl={6} className="emailcontainer">
-      <html for="email la">Email Id</html>
-      <input className='emailinput' type="email"
-      value={email}
-      onChange={(event)=>setEmail(event.target.value)}/>
-    </Col>
-  </Row>
-  <Row>
-    <Col xl={6} className="Jobcontainer">
-      <html for="Jobtype la">Job Type</html>
-      <Select
-          className='input'
-          labelId="demo-simple-select-required-html"
-          id="demo-simple-select-required"
-          value={jobtype}
-          html="Age *"
-          onChange={(event)=>setJobtype(event.target.value)}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={"Full Time"}>Full Time</MenuItem>
-          <MenuItem value={"Part Time"}>Part Time</MenuItem>
-          <MenuItem value={"Consultancy"}>Consultancy</MenuItem>
-        </Select>
-    
-    </Col>
-    <Col xl={6} className="dobcontainer">
-      <html for="dob la"> DOB</html>
-      <input className='dob' type="date"
-      value={dob}
-       onChange={(a)=>setDob(a.target.value)}/>
-    </Col>
-  </Row>
-  
-  <Row>
-    <Col xl={6} className="locationcontainer input">
-      <html for="location la">Pref. Location</html>
-      <Select
-          className='input'
-          labelId="demo-simple-select-required-html"
-          id="demo-simple-select-required"
-          value={location}
-          html="Age *"
-          onChange={(event)=>setLocation(event.target.value)}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={"chennai"}>Chennai</MenuItem>
-          <MenuItem value={"Bangalore"}>Bangalore</MenuItem>
-          <MenuItem value={"Coimbatore"}>Coimbatore</MenuItem>
-        </Select>
-     
-    </Col>
-    <Col xl={6} className="">
-    <Button variant="contained" onClick={addemployee}>+Add/Update</Button>
-    </Col>
-  </Row>
-  
+    <div className="Form">
+      <Form>
+        <fieldset className="fieldset">
+          <legend className="legend">Registration</legend>
+          <Container>
+            <Row>
+              <Col xl={6} className="fullnamecontainer">
+                <label htmlFor="fullname la">Fullname</label>
+                <input
+                  className="fullname"
+                  type="text"
+                  value={fullname}
+                  onChange={(a) => setFullname(a.target.value)}
+                />
+              </Col>
+              <Col xl={6} className="img">
+                <label htmlFor="profilepic la">Profile Pic</label>
+                <Card className="imagebox">
+                  <img className="image" src={img} alt="profilepic" />
+                </Card>
+                <input
+                  type="file"
+                  accept="image/*"
+                  value={img}
+                  onChange={(a) => setImg(a.target.value)}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col xl={6} className="mobilecontainer input">
+                <label htmlFor="mobile la">Mobile</label>
+                <Select
+                  labelId="demo-simple-select-required-html"
+                  id="demo-simple-select-required"
+                  value={countrycode}
+                  html="Age *"
+                  onChange={(event) => setCountrycode(event.target.value)}
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={+91}>+91</MenuItem>
+                  <MenuItem value={+47}>+47</MenuItem>
+                  <MenuItem value={+385}>+385</MenuItem>
+                </Select>
+                <input
+                  className="Mobile"
+                  type="text"
+                  value={mobile}
+                  onChange={(a) => setMobile(a.target.value)}
+                />
+              </Col>
+              <Col xl={6} className="emailcontainer">
+                <label htmlFor="email la">Email Id</label>
+                <input
+                  className="emailinput"
+                  type="email"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col xl={6} className="Jobcontainer">
+                <label htmlFor="Jobtype la">Job Type</label>
+                <Select
+                  className="input"
+                  labelId="demo-simple-select-required-html"
+                  id="demo-simple-select-required"
+                  value={jobtype}
+                  html="Age *"
+                  onChange={(event) => setJobtype(event.target.value)}
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={"Full Time"}>Full Time</MenuItem>
+                  <MenuItem value={"Part Time"}>Part Time</MenuItem>
+                  <MenuItem value={"Consultancy"}>Consultancy</MenuItem>
+                </Select>
+              </Col>
+              <Col xl={6} className="dobcontainer">
+                <label htmlFor="dob la"> DOB</label>
+                <input
+                  className="dob"
+                  type="date"
+                  value={dob}
+                  onChange={(a) => setDob(a.target.value)}
+                />
+              </Col>
+            </Row>
 
-  </Container>
-  </fieldset>
-  <Table/>
-</Form>
-
+            <Row>
+              <Col xl={6} className="locationcontainer input">
+                <label htmlFor="location la">Pref. Location</label>
+                <Select
+                  className="input"
+                  labelId="demo-simple-select-required-html"
+                  id="demo-simple-select-required"
+                  value={location}
+                  html="Age *"
+                  onChange={(event) => setLocation(event.target.value)}
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={"chennai"}>Chennai</MenuItem>
+                  <MenuItem value={"Bangalore"}>Bangalore</MenuItem>
+                  <MenuItem value={"Coimbatore"}>Coimbatore</MenuItem>
+                </Select>
+              </Col>
+              <Col xl={6} className="">
+                <Button variant="contained" onClick={addemployee}>
+                  +Add/Update
+                </Button>
+              </Col>
+            </Row>
+          </Container>
+        </fieldset>
+        <Table />
+      </Form>
     </div>
-  )
+  );
 }
--- src/Component/Table.js
+++ src/Component/Table.js
@@ -1,73 +1,75 @@
-import React from 'react'
-import './Table.css';
-import{useEffect,useState} from 'react';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import "./Table.css";
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 export default function Table() {
-  const [employee,setEmployee]=useState([]);
-  const history=useHistory()
-  const getEmployee=()=>{
-    fetch(`https://62368e85163bf7c4746552f6.mockapi.io/employee`)
-  // fetch(`https://paripornaform.herokuapp.com/employee`)
-    .then((data)=>data.json())
-    .then((a)=>setEmployee(a.data))
-    console.log("employee",employee)
-  }
-  useEffect(getEmployee,[])
+  const [employee, setEmployee] = useState([]);
+  const history = useHistory();
+  const getEmployee = () => {
+    // fetch(`https://62368e85163bf7c4746552f6.mockapi.io/employee`)
+    fetch(`https://paripornaform.herokuapp.com/employee`)
+      .then((data) => data.json())
+      .then((a) => setEmployee(a.data));
+    console.log("employee", employee);
+  };
+  useEffect(getEmployee, []);
   const deleteEmployee = (id) => {
-    //fetch(`https://paripornaform.herokuapp.com/employee/${id}`,{
-   fetch(`https://62368e85163bf7c4746552f6.mockapi.io/employee/${id}`,{
-      method:"DELETE",
-  }).then(()=>getEmployee());
-    };
+    fetch(`https://paripornaform.herokuapp.com/employee/${id}`,{
+    //fetch(`https://62368e85163bf7c4746552f6.mockapi.io/employee/${id}`, {
+      method: "DELETE",
+    }).then(() => getEmployee());
+  };
 
   return (
-    <div className='table-container'>
-<table>
-    <thead>
-        <tr>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Mobile</th>
-        <th>DOB</th>
-        <th>Job Type</th>
-        <th>Action</th>
-        </tr>
-    </thead>
-    <tbody>
-      {employee.map(({_id,fullname,mobile,email,jobtype,dob,location})=>(
-        
-        <tr>
-        <td>{fullname}</td>
-        <td>{email}</td>
-        <td>{mobile}</td>
-        <td>{dob}</td>
-        <td>{jobtype}</td>
-        <td> <button  
-        className="editbutton"
-                      onClick={()=>{
-                        console.log(_id)
-                        history.push(`employee/edit/${_id}`)
-                      }}
-                    >
-                      Edit
-                    </button>
-                    <button
-                      type="button"
-                      onClick={() => deleteEmployee(_id)}
-                      className="deletebutton"
-                      style={{marginLeft:"20px"}}
-                    >
-                      Delete
-                    </button></td>
-        </tr>
-      ))
-}
-    </tbody>
-</table>
-
+    <div className="table-container">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Mobile</th>
+            <th>DOB</th>
+            <th>Job Type</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employee.map(
+            ({ _id, fullname, mobile, email, jobtype, dob, location }) => (
+              <tr>
+                <td>{fullname}</td>
+                <td>{email}</td>
+                <td>{mobile}</td>
+                <td>{dob}</td>
+                <td>{jobtype}</td>
+                <td>
+                  {" "}
+                  <button
+                    className="editbutton"
+                    onClick={() => {
+                      console.log(_id);
+                      history.push(`employee/edit/${_id}`);
+                    }}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => deleteEmployee(_id)}
+                    className="deletebutton"
+                    style={{ marginLeft: "20px" }}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            )
+          )}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
